8638e2edbd0b941c8d3e4107aeeb5a0ecf4b742d
noemiRE <noemifelicitaramos@gmail.com>
2021-03-04 19:28:26 -0500

    responsive de la seccion principal terminada

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const qs = document.querySelector.bind(document)
+qs('#menu button').addEventListener('click',showMenu)
+
+function showMenu(){
+    qs('#menu ul').classList.toggle("h-full")
+    qs('#menu ul').classList.toggle("ul-full")
+    // qs('#menu ul').classList.toggle("contract-menu")
+    qs('#menu').classList.toggle("bg-dark-light")
+    qs('#menu').classList.toggle("expand-menu")
+    qs('#menu button').classList.toggle("bg-close")
+    // qs('#menu').classList.toggle("contract-menu")
+}
+
+
